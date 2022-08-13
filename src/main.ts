3218c706899e73b2f790b8b8e56520095b9063b6
Micheal Parks <michealparks1989@gmail.com>
2022-08-13 16:42:59 -0400

    add math functions

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,2 @@
 export * from './lib'
+export * as math from './math'
---
+++ src/math/index.ts
@@ -0,0 +1,78 @@
+/**
+ * Generates a random float in [-range, range]
+ * 
+ * @param range The range
+ * @returns A random number in the range
+ */
+export const randomNumber = (range: number): number => {
+  return Math.random() * range * 2 - range
+}
+
+/**
+ * Generates a random float between a min and max
+ * 
+ * @param min The minimum number
+ * @param max The maximum number
+ * @returns A random number between two numbers
+ */
+export const randomNumberBetween = (min: number, max: number): number => {
+  return Math.random() * (max - min) + min
+}
+
+/**
+ * Generates a random 2d point within a circle
+ * 
+ * @param R The radius of the circle
+ * @returns A random point
+ */
+export const randomPointInCircle = (R: number): [x: number, y: number] => {
+  const r = R * Math.sqrt(Math.random())
+  const theta = Math.random() * 2 * Math.PI
+  const x = r * Math.cos(theta)
+  const y = r * Math.sin(theta)
+  return [x, y]
+}
+
+/**
+ * Generates a random 3d point within a sphere
+ * 
+ * @param R The radius of the sphere
+ * @returns A random point
+ */
+export const randomPointInSphere = (R: number): [x: number, y: number, z: number] => {
+  const u = Math.random()
+  const v = Math.random()
+  const theta = u * 2 * Math.PI
+  const phi = Math.acos(2 * v - 1)
+  const r = Math.cbrt(Math.random() * R)
+  const sinTheta = Math.sin(theta)
+  const cosTheta = Math.cos(theta)
+  const sinPhi = Math.sin(phi)
+  const cosPhi = Math.cos(phi)
+  const x = r * sinPhi * cosTheta
+  const y = r * sinPhi * sinTheta
+  const z = r * cosPhi
+  return [x, y, z]
+}
+
+/**
+ * Generates a random 3d point on the surface of a sphere
+ * 
+ * @param R The radius of the sphere
+ * @returns A random point
+ */
+export const randomPointOnSphere = (R: number): [x: number, y: number, z: number] => {
+  const u = Math.random()
+  const v = Math.random()
+  const theta = u * 2 * Math.PI
+  const phi = Math.acos(2 * v - 1)
+  const r = Math.cbrt(Math.random() + R)
+  const sinTheta = Math.sin(theta)
+  const cosTheta = Math.cos(theta)
+  const sinPhi = Math.sin(phi)
+  const cosPhi = Math.cos(phi)
+  const x = r * sinPhi * cosTheta
+  const y = r * sinPhi * sinTheta
+  const z = r * cosPhi
+  return [x, y, z]
+}
