7dd110388fd3fbb3b08615c10af572cc45d1d0c7
Micheal Parks <michealparks1989@gmail.com>
2022-09-18 14:38:39 -0400

    add zero vector

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,7 @@
 import * as THREE from 'three'
 
-export const UP = new THREE.Vector3(0, 1, 0)
-export const FORWARD = new THREE.Vector3(0, 0, -1)
+export const vector = {
+  FORWARD: new THREE.Vector3(0, 0, -1),
+  UP: new THREE.Vector3(0, 1, 0),
+  ZERO: new THREE.Vector3(0, 0, 0),
+}
